bc2824aaff2c43a81b778b195b893855b39f6c6c
Ngayka <ngayka@gmail.com>
2025-07-26 17:22:19 +0300

    added book model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,21 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    class StatusChoices(models.TextChoices):
+        soft = "Soft"
+        hard = "Hard"
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=100, choices = StatusChoices.choices)
+    inventory = models.PositiveIntegerField(
+        validators=[MinValueValidator(0)],
+        help_text="Number of available books"
+    )
+    daily_fee = models.DecimalField(
+        max_digits=5, decimal_places=2,
+        validators=[MinValueValidator(Decimal('0.00'))]
+    )
