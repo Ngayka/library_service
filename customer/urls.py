d17fdeb26576a77c889ebbf2576681bbd3793e7f
Ngayka <ngayka@gmail.com>
2025-07-26 17:09:20 +0300

    added customer urls, fixed customer serializer

---
+++ customer/urls.py
@@ -0,0 +1,17 @@
+from customer.views import CreateUserView, ManageUserView
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
+app_name = "customer"
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create_user"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
